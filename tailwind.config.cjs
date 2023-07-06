45d633dca321ee7cb856837b694c76269bea19c2
Cristhian Melo <rxtsel@hotmail.com>
2023-07-06 12:15:59 -0500

    chore: add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        cYellow: "#fcea0e",
+        cBlack: "#1d1d1b",
+      },
+    },
+  },
+  plugins: [],
+};
